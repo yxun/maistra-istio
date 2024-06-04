maistra/istio

Make default gateway class and controller names configurable
* gw-api: make default class name and controller name configurable

This is really useful in multi-tenant/multi-revision scenarios

Co-authored-by: Daniel Grimm <dgrimm@redhat.com>
Signed-off-by: Yann Liu <yannliu@redhat.com>

* gw-api: use consistent controller name

Co-authored-by: Daniel Grimm <dgrimm@redhat.com>
Signed-off-by: Yann Liu <yannliu@redhat.com>

pilot/pkg/config/kube/gateway/gatewayclass_test.go
@@ -22,7 +22,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	gateway "sigs.k8s.io/gateway-api/apis/v1beta1"
 
-	"istio.io/istio/pkg/config/constants"
 	"istio.io/istio/pkg/kube"
 	"istio.io/istio/pkg/kube/kclient/clienttest"
 	"istio.io/istio/pkg/test"
@@ -71,19 +70,19 @@ func TestClassController(t *testing.T) {
 	}
 
 	// Class should be created initially
-	expectClass(DefaultClassName, constants.ManagedGatewayController)
+	expectClass(string(DefaultClassName), string(ControllerName))
 
 	// Once we delete it, it should be added back
-	deleteClass(DefaultClassName)
-	expectClass(DefaultClassName, constants.ManagedGatewayController)
+	deleteClass(string(DefaultClassName))
+	expectClass(string(DefaultClassName), string(ControllerName))
 
 	// Overwrite the class, controller should not reconcile it back
-	createClass(DefaultClassName, "different-controller")
-	expectClass(DefaultClassName, "different-controller")
+	createClass(string(DefaultClassName), "different-controller")
+	expectClass(string(DefaultClassName), "different-controller")
 
 	// Once we delete it, it should be added back
-	deleteClass(DefaultClassName)
-	expectClass(DefaultClassName, constants.ManagedGatewayController)
+	deleteClass(string(DefaultClassName))
+	expectClass(string(DefaultClassName), string(ControllerName))
 
 	// Create an unrelated GatewayClass, we should not do anything to it
 	createClass("something-else", "different-controller")

yxun/maistra-istio

pilot/pkg/config/kube/gateway/gatewayclass_test.go
@@ -22,7 +22,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	gateway "sigs.k8s.io/gateway-api/apis/v1beta1"
 
-	"istio.io/istio/pkg/config/constants"
 	"istio.io/istio/pkg/kube"
 	"istio.io/istio/pkg/kube/kclient/clienttest"
 	"istio.io/istio/pkg/test"
@@ -71,19 +70,19 @@ func TestClassController(t *testing.T) {
 	}
 
 	// Class should be created initially
-	expectClass(defaultClassName, constants.ManagedGatewayController)
+	expectClass(string(defaultClassName), string(controllerName))
 
 	// Once we delete it, it should be added back
-	deleteClass(defaultClassName)
-	expectClass(defaultClassName, constants.ManagedGatewayController)
+	deleteClass(string(defaultClassName))
+	expectClass(string(defaultClassName), string(controllerName))
 
 	// Overwrite the class, controller should not reconcile it back
-	createClass(defaultClassName, "different-controller")
-	expectClass(defaultClassName, "different-controller")
+	createClass(string(defaultClassName), "different-controller")
+	expectClass(string(defaultClassName), "different-controller")
 
 	// Once we delete it, it should be added back
-	deleteClass(defaultClassName)
-	expectClass(defaultClassName, constants.ManagedGatewayController)
+	deleteClass(string(defaultClassName))
+	expectClass(string(defaultClassName), string(controllerName))
 
 	// Create an unrelated GatewayClass, we should not do anything to it
 	createClass("something-else", "different-controller")