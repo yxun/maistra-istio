maistra/istio

OSSM-4880: Rebase building OpenSSL-based proxy image (#860)
* OSSM-2378: Use Maistra proxy CentOS Stream 8 img (#713)

* OSSM-2378: Use Maistra proxy CentOS Stream 8 img

For integration tests. Based on (#609)

* Build container images with docker

Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>

* Use maistra proxy and CentOS Stream 8 as a base image for proxyv2

Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>

* Test VMs using CentOS Stream 8

Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>

* Temporarily skip failing security tests

Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>

* Temporarily skip tests suite telemetry.prometheus.wasm

Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>

Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>

* fix(tests): enables fixed integration tests

Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>
Co-authored-by: Jacek Ewertowski <jewertow@redhat.com>

* Use our proxy (#801)

Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>

* OSSM-5958: Remove wasm extensions (#937)

* Do not rely on WASM extensions

Since https://github.com/maistra/proxy/pull/253

* Remove wasm extensions from EnvoyFilters

Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>

* Disable tests for WASM extensions

Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>

* Remove commented code

Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>

---------

Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>
Co-authored-by: Jonh Wendell <jwendell@redhat.com>

* Disable failing tests

Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>

* Fix TestTrustDomainValidation - change expected TLS error to make it work with OpenSSL

Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>

---------

Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>
Co-authored-by: Bartosz Majsak <bartosz.majsak@gmail.com>
Co-authored-by: Jonh Wendell <jonh.wendell@redhat.com>
Co-authored-by: Jonh Wendell <jwendell@redhat.com>

pkg/test/framework/components/echo/config.go
@@ -70,13 +70,14 @@ type Configurable interface {
 type VMDistro = string
 
 const (
-	UbuntuXenial VMDistro = "UbuntuXenial"
-	UbuntuJammy  VMDistro = "UbuntuJammy"
-	Debian11     VMDistro = "Debian9"
-	Centos7      VMDistro = "Centos7"
-	Rockylinux8  VMDistro = "Centos8"
+	UbuntuXenial  VMDistro = "UbuntuXenial"
+	UbuntuJammy   VMDistro = "UbuntuJammy"
+	Debian11      VMDistro = "Debian9"
+	Centos7       VMDistro = "Centos7"
+	Rockylinux8   VMDistro = "Centos8"
+	CentosStream8 VMDistro = "CentosStream8"
 
-	DefaultVMDistro = UbuntuJammy
+	DefaultVMDistro = CentosStream8
 )
 
 // Config defines the options for creating an Echo component.

pkg/test/framework/components/echo/kube/deployment.go
@@ -267,10 +267,11 @@ func GenerateService(cfg echo.Config) (string, error) {
 }
 
 var VMImages = map[echo.VMDistro]string{
-	echo.UbuntuXenial: "app_sidecar_ubuntu_xenial",
-	echo.UbuntuJammy:  "app_sidecar_ubuntu_jammy",
-	echo.Debian11:     "app_sidecar_debian_11",
-	echo.Centos7:      "app_sidecar_centos_7",
+	echo.UbuntuXenial:  "app_sidecar_ubuntu_xenial",
+	echo.UbuntuJammy:   "app_sidecar_ubuntu_jammy",
+	echo.Debian11:      "app_sidecar_debian_11",
+	echo.Centos7:       "app_sidecar_centos_7",
+	echo.CentosStream8: "app_sidecar_centos_stream_8",
 	// echo.Rockylinux8:  "app_sidecar_rockylinux_8", TODO(https://github.com/istio/istio/issues/38224)
 }
 

yxun/maistra-istio

pkg/test/framework/components/echo/config.go
@@ -70,12 +70,13 @@ type Configurable interface {
 type VMDistro = string
 
 const (
-	UbuntuBionic VMDistro = "UbuntuBionic"
-	UbuntuJammy  VMDistro = "UbuntuJammy"
-	Debian11     VMDistro = "Debian11"
-	Rockylinux8  VMDistro = "Centos8"
+	UbuntuXenial  VMDistro = "UbuntuBionic"
+	UbuntuJammy   VMDistro = "UbuntuJammy"
+	Debian11      VMDistro = "Debian11"
+	Rockylinux8   VMDistro = "Centos8"
+	CentosStream8 VMDistro = "CentosStream8"
 
-	DefaultVMDistro = UbuntuJammy
+	DefaultVMDistro = CentosStream8
 )
 
 // Config defines the options for creating an Echo component.

pkg/test/framework/components/echo/kube/deployment.go
@@ -267,9 +267,10 @@ func GenerateService(cfg echo.Config) (string, error) {
 }
 
 var VMImages = map[echo.VMDistro]string{
-	echo.UbuntuBionic: "app_sidecar_ubuntu_bionic",
-	echo.UbuntuJammy:  "app_sidecar_ubuntu_jammy",
-	echo.Debian11:     "app_sidecar_debian_11",
+	echo.UbuntuXenial:  "app_sidecar_ubuntu_bionic",
+	echo.UbuntuJammy:   "app_sidecar_ubuntu_jammy",
+	echo.Debian11:      "app_sidecar_debian_11",
+	echo.CentosStream8: "app_sidecar_centos_stream_8",
 	// echo.Rockylinux8:  "app_sidecar_rockylinux_8", TODO(https://github.com/istio/istio/issues/38224)
 }
 